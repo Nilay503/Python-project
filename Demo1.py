0a76ed4baba90c0dfd89b19f15f6510d4515b9a8
Nilay503 <nilay.shukla06@gmail.com>
2026-01-08 21:13:26 +0530

    Add files via upload

---
+++ Demo1.py
@@ -0,0 +1,10 @@
+import qrcode
+url = input("Enter the url: ").strip()
+file_path =  "qrcode.png"
+qr = qrcode.QRCode()
+qr.add_data(url)
+
+img = qr.make_image()
+img.save(file_path)
+
+print("qr code is generated")
